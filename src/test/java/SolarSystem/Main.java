82cfad990d9562eb802dbdaf0c982eed6d2e9752
avismm <amuhwerane@gmail.com>
2023-11-02 06:22:57 +0700

    this commot has the basic structure of the solar system

---
+++ src/test/java/SolarSystem/Main.java
@@ -0,0 +1,12 @@
+package SolarSystem;
+
+public class Main {
+    public static void main(String[] args) {
+        SolarSystemBasics solarSys=new SolarSystemBasics();
+        solarSys.sun="1";
+        solarSys.moon="2";
+        solarSys.stars="2";
+        solarSys.planets="10";
+
+    }
+}
---
+++ src/test/java/SolarSystem/SolarSystemBasics.java
@@ -0,0 +1,8 @@
+package SolarSystem;
+
+public class SolarSystemBasics {
+    String sun;
+    String moon;
+    String stars;
+    String planets;
+}
